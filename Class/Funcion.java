7ad1d30ace175501e33d95377b1db164b4dc2b7e
OsvaldoPachecoSanabria <osvaldo.pachecosanabria@unicolombo.edu.co>
2025-03-09 13:27:03 -0500

    Creacion De clase Funcion y Clase Pelicula para que tenga la excistencia de este y el agregadoo para la funcion sepa que pelicula se va a transmitir pero solo esta seteada

---
+++ Class/Funcion.java
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime;
+
+public class Funcion {
+    private Pelicula pelicula;
+    private LocalDateTime horario;
+
+    public Funcion(Pelicula pelicula, LocalDateTime horario) {
+        this.pelicula = pelicula;
+        this.horario = horario;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcion{" +
+                "pelicula=" + pelicula +
+                ", horario=" + horario +
+                '}';
+    }
+}
